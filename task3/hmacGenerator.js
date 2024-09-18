71defbc3debf0386e40c9e9ea460baf05301151d
Artur <86933627+Artur050@users.noreply.github.com>
2024-09-18 09:59:05 +0500

    Delete task3 directory

--- task3/hmacGenerator.js
+++
@@ -1,9 +0,0 @@
-import crypto from 'crypto'
-
-class HmacGenerator {
-    static generateHmac(key, message) {
-        return crypto.createHmac('sha256', key).update(message).digest('hex');
-    }
-}
-
-export default HmacGenerator;
--- task3/keyGenerator.js
+++
@@ -1,9 +0,0 @@
-import crypto from 'crypto';
-
-class KeyGenerator {
-    static generateKey() {
-        return crypto.randomBytes(32).toString('hex');
-    }
-}
-
-export default KeyGenerator;
